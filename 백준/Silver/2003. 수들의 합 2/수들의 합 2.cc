b0128b5a48c7e743aef91321d8cc3c424b647042
jihodoo <160157705+jihodoo@users.noreply.github.com>
2024-06-28 02:12:27 +0900

    [Silver IV] Title: 수들의 합 2, Time: 4 ms, Memory: 2152 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.cc
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.cc
@@ -1,3 +1,4 @@
+//using two pointer
 #include <iostream>
 #include <vector>
 
@@ -13,17 +14,37 @@ int main() {
 		A.push_back(a);
 	}
 	int count = 0;
-	for (int i = 0; i < N; i++) {
-		int c = 0;
-		for (int j = i; j <N; j++) {
-			c = c + A[j];
-			if (c == M) { 
-				count++;
-				break;
+	int first = 0;
+	int second = 0;
+	int c = A[first];
+	while (first <= second && second < N) {
+		if (c < M) {
+			if (second < N-1) {
+				second++;
+				c = c + A[second];
 			}
-			if (c > M) break;
+			else
+				break;
+			
+		}
+		else if (c == M) {
+			count++;
+            second++;
+			if (second < N) {
+				c = c + A[second];
+			}
+
+		}
+		else if (c > M) {
+			c = c - A[first];
+			first++;
+			if (first > second) {
+				second = first;
+				c = A[first];
+			}
 		}
 	}
+	
 	cout << count;
 
 	return 0;
