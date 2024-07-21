733e6c3c03aadaf73629b4df7e7ba90ba9aa5e13
jihodoo <160157705+jihodoo@users.noreply.github.com>
2024-07-22 03:38:34 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string a;
+    cin >> a;
+    string b = ":fan:";
+    cout << b << b << b << "\n";
+    cout << b <<":"<< a <<":" << b << "\n";
+    cout << b << b << b << "\n";
+
+    return 0;
+}
