4d6f7fe72e2a607219f30416dd4080549e5bfafb
jihodoo <160157705+jihodoo@users.noreply.github.com>
2024-08-05 22:10:43 +0900

    [Silver II] Title: 최대 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int N;
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    priority_queue<int> pq;
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        int a;
+        cin >> a;
+        if (a) {
+            pq.push(a);
+        }
+        else {
+            if (pq.empty()) cout << 0 << "\n";
+            else {
+                cout << pq.top() << "\n";
+                pq.pop();
+            }
+        }
+    }
+    return 0;
+}
