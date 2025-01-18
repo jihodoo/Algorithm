a7bfc91cb661b94103b19691967c7fac7d837d5c
jihodoo <160157705+jihodoo@users.noreply.github.com>
2025-01-18 20:07:37 +1000

    [Silver III] Title: 수열, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/2559. 수열/수열.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int N, K; int tmp;
+int psum[100001];
+int total = -100*100000 -1;  //최악의 상황일때 최솟값
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	cin >> N >> K;
+	for (int i = 1; i <= N; i++) {
+		cin >> tmp;
+		psum[i] = psum[i - 1] + tmp;
+	}
+	for (int i = K; i <= N; i++) {
+		if (total < psum[i] - psum[i - K]) total = psum[i] - psum[i - K];
+	}
+	cout << total;
+	return 0;
+}
