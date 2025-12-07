3e61892384c46acfe733159329d66c47d608bdf1
jihodoo <160157705+jihodoo@users.noreply.github.com>
2025-12-07 21:00:58 +0900

    [Bronze II] Title: 트럭 주차, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2979. 트럭 주차/트럭 주차.cc
+++ 백준/Bronze/2979. 트럭 주차/트럭 주차.cc
@@ -1,34 +1,30 @@
 #include <iostream>
-
 using namespace std;
-int A, B, C;
-int a1, a2, a3, b1, b2, b3;
-int sum;
-int cnt[101];
 
-void park(int a, int b) {
-	for (int i = a; i < b; i++) {
-		cnt[i]++;
-	}
-}
-
+int  A, B, C;
+int sum = 0;
+int times[101] = { 0 };
 int main() {
-	cin >> A >> B >> C;
-	cin >> a1 >> b1 >> a2 >> b2 >> a3 >> b3;
-	park(a1, b1);
-	park(a2, b2);
-	park(a3, b3);
-	for (int i : cnt) {
-		if (i == 1) sum += A;
-		if (i == 2) sum += 2 * B;
-		if (i == 3) sum += 3 * C;
-	}
-	cout << sum;
+    cin >> A >> B >> C;
+    for (int i = 0; i < 3; i++) {
+        int a, b;
+        cin >> a >> b;
+        for (int i = a; i < b; i++) {
+            times[i] ++;
+        }
+    }
+    for (int i : times) {
+        if (i == 1) {
+            sum += i * A;
+        }
+        else if (i == 2) {
+            sum += i * B;
+        }
+        else if (i == 3) {
+            sum += i * C;
+        }
+    }
 
-	return 0;
+    cout << sum;
+    return 0;
 }
-
-/*1대 -> A 2대 -> 2B, 3대 -> 3C
-1 2 3 4 5 6 7 8
-1 2 3 3 2 1 1 
-5 6 3 3 6 5 5  15 12 6 27 +6 = 33*/
