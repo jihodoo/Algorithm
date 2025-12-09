71e194fa8f4d62f99d5ec1572ca18ba9c30a2382
jihodoo <160157705+jihodoo@users.noreply.github.com>
2025-12-09 18:28:25 +0900

    Update ROT13_25ver.cc

--- 백준/Bronze/11655. ROT13/ROT13_25ver.cc
+++ 백준/Bronze/11655. ROT13/ROT13_25ver.cc
@@ -1,1 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+string s;
+int main() {
+    getline(cin, s);
+
+    for (char c : s) {
+        if (c >= 'A' && c <= 'A' + 12) cout << char(c + 13);
+        else if (c >= 'A' + 13 && c <= 'Z') cout << char(c - 13);
+        else if (c >= 'a' && c <= 'a' + 12) cout << char(c + 13);
+        else if (c >= 'a' + 13 && c <= 'z') cout << char(c - 13);
+        else cout << c;
+    }
+    return 0;
+}
