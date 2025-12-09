544f9d95e577ac1f2f1de088c14d89664520b18a
jihodoo <160157705+jihodoo@users.noreply.github.com>
2025-12-10 02:43:43 +0900

    [Silver IV] Title: 좋은 단어, Time: 16 ms, Memory: 2268 KB -BaekjoonHub

--- 백준/Silver/3986. 좋은 단어/좋은 단어.cc
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.cc
@@ -1,33 +1,21 @@
 #include <iostream>
 #include <stack>
-
 using namespace std;
 
-int N;
-string s;
-
-
+int N, cnt;
 int main() {
-	cin >> N;
-	int cnt = 0;
-
-	for (int i = 0; i < N; i++) {
-		stack<int> st;
-		cin >> s;
-		if (s.size() & 1) continue;
-		st.push(s[0]);
-		for (int j = 1; j < s.size(); j++) {
-			if (st.empty()) {
-				st.push(s[j]);
-				continue;
-			}
-			if (st.top() == s[j]) st.pop();
-			else {
-				st.push(s[j]);
-			}
-		}
-		if (st.empty()) cnt++;
-	}
-	cout << cnt << "\n";
-	return 0;
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        stack<char> st;
+        string s;
+        cin >> s;
+        for (int j = 0; j < s.size(); j++) {
+            if (st.empty()) st.push(s[j]);
+            else if (st.top() == s[j]) st.pop();
+            else st.push(s[j]);
+        }
+        if (st.empty()) cnt++;
+    }
+    cout << cnt;
+    return 0;
 }
