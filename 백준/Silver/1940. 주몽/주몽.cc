4b87e9536ecb25780778814326cc000e90802c6d
jihodoo <160157705+jihodoo@users.noreply.github.com>
2025-12-10 01:16:07 +0900

    [Silver IV] Title: 주몽, Time: 88 ms, Memory: 2152 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.cc
+++ 백준/Silver/1940. 주몽/주몽.cc
@@ -2,23 +2,22 @@
 #include <vector>
 using namespace std;
 
-int N, M, cnt;
-vector<int> num;
-
+int N, M;
+int cnt;
+vector<int> v;
 int main() {
-	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-	cin >> N >> M;
-	for (int i = 0; i < N; i++) {
-		int a;
-		cin >> a;
-		num.push_back(a);
-	}
-	for (int i = 0; i < num.size() -1 ; i++) {
-		for (int j = i + 1; j < num.size(); j++) {
-			if (num[i] + num[j] == M) cnt++;
-		}
-	}
-	cout << cnt << "\n";
-
-	return 0;
+    cin >> N >> M;
+    for (int i = 0; i < N; i++) {
+        int a;
+        cin >> a;
+        v.push_back(a);
+    }
+    for (int i = 0; i < N; i++) {
+        for (int j = i + 1; j < N; j++) {
+            int sum = v[i] + v[j];
+            if (sum == M) cnt++;
+        }
+    }
+    cout << cnt;
+    return 0;
 }
